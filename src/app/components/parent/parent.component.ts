c4a68bcdea1e66615890113312e039344444c176
Abu_Programmiy <abdukholiq23@gmail.com>
2025-03-20 09:36:51 +0500

    making clear

--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -10,28 +10,5 @@ import { UserModule } from '../../modules/user/user.module';
   styleUrl: './parent.component.scss',
 })
 export class ParentComponent {
-  currentComponent = HelloComponent
-
-  @ViewChild('container', { read: ViewContainerRef, static: true }) container!: ViewContainerRef;
-  componentRef!: ComponentRef<HelloComponent>
-  // ^^^ -> componentRef elon qilish
-
-  ngOnInit(): void {
-    this.container.createComponent(HelloComponent)
-  }
-
-  changeComponent(name: string) {
-    if (name == "hello") {
-      this.container.clear()
-      this.componentRef = this.container.createComponent(HelloComponent)
-      // ^^^ -> reference berish
-      this.componentRef.instance.message = "Hello from parent to input"
-      // ^^^ -> inputini update qilish
-
-    }
-    else if (name == "goodbye") {
-      this.container.clear()
-      this.container.createComponent(GoodbyeComponent)
-    }
-  }
+  
 }
